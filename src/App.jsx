e63730f500bbccf792fa20ff5785a552d1b15196
LittleCreamSoda <nikitalahin@gmail.com>
2023-12-25 09:24:56 +0400

    Add prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
-import './App.css'
+import "./App.css";
 
-const App = () =>  {
-
+const App = () => {
   return (
-    <div className={'initial-message__container'}>
-      <p className={'initial-message__p'}>
-          This project was created for the live coding sessions for the Uploadcare interview process.
-          The problem you can find{' '}
-          <a className={'initial-message__link'} href="https://excalidraw.com/#json=-e-nyTdhBkpmfG5ws1BUA,zVLzpw0fbIIBQMpKACmGZQ"
-                                   target={'_blank'} rel="noreferrer">
-          here</a>.
+    <div className="initial-message__container">
+      <p className="initial-message__p">
+        This project was created for the live coding sessions for the Uploadcare interview process.
+        The problem you can find&nbsp;
+        <a
+          className="initial-message__link"
+          href="https://excalidraw.com/#json=-e-nyTdhBkpmfG5ws1BUA,zVLzpw0fbIIBQMpKACmGZQ"
+          target="_blank"
+          rel="noreferrer"
+        >
+          here
+        </a>
+        .
       </p>
     </div>
-  )
+  );
+};
 
-}
-
-export default App
+export default App;
