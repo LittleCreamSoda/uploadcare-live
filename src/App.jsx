76d07ac1471f11b90b5f4c0fb7cd00332b2414d9
LittleCreamSoda <nikitalahin@gmail.com>
2023-12-25 09:19:23 +0400

    Setup the initial screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () =>  {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className={'initial-message__container'}>
+      <p className={'initial-message__p'}>
+          This project was created for the live coding sessions for the Uploadcare interview process.
+          The problem you can find{' '}
+          <a className={'initial-message__link'} href="https://excalidraw.com/#json=-e-nyTdhBkpmfG5ws1BUA,zVLzpw0fbIIBQMpKACmGZQ"
+                                   target={'_blank'} rel="noreferrer">
+          here</a>.
       </p>
-    </>
+    </div>
   )
+
 }
 
 export default App
