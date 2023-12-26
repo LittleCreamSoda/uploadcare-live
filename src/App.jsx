167bd957ea15f8279b490e66a97bcd5f4de5e20d
LittleCreamSoda <nikitalahin@gmail.com>
2023-12-26 14:05:41 +0400

    Change the problem link

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         The problem you can find&nbsp;
         <a
           className="initial-message__link"
-          href="https://excalidraw.com/#json=-e-nyTdhBkpmfG5ws1BUA,zVLzpw0fbIIBQMpKACmGZQ"
+          href="https://excalidraw.com/#room=6b9a93d92b67f5a8fe11,QTPoqTe3Z0yoQ4btLJ9SOw"
           target="_blank"
           rel="noreferrer"
         >
